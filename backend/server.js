fae3dde18c6e72e622e07fb5c9afdc6cd63d12cb
Sakkyx <keigosasaki99@gmail.com>
2026-05-26 00:10:44 -0300

    Atualizando integração frontend e backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
+require('dotenv').config()
+
+const Cliente = require('./models/Cliente')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('API funcionando!')
+})
+
+app.post('/clientes', async (req, res) => {
+  try {
+    const cliente = await Cliente.create(req.body)
+    res.status(201).json(cliente)
+  } catch (error) {
+    res.status(500).json({ erro: error.message })
+  }
 })
 
 app.listen(3000, () => {
--- frontend/src/pages/cadastro.jsx
+++ frontend/src/pages/cadastro.jsx
@@ -1,14 +1,64 @@
+import { useState } from 'react'
+import axios from 'axios'
+
 export default function Cadastro() {
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  async function cadastrar(e) {
+    e.preventDefault()
+
+    try {
+      await axios.post(
+        'https://restauranteakps.onrender.com/clientes',
+        {
+          nome,
+          email,
+          senha
+        }
+      )
+
+      alert('Cliente cadastrado!')
+
+      setNome('')
+      setEmail('')
+      setSenha('')
+    } catch (error) {
+      alert('Erro ao cadastrar')
+      console.log(error)
+    }
+  }
+
   return (
     <div className="container">
-      <h1>Cadastro de Cliente</h1>
+      <h1>Cadastro</h1>
 
-      <form>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+      <form onSubmit={cadastrar}>
+        <input
+          type="text"
+          placeholder="nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
 
-        <button>Cadastrar</button>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
       </form>
     </div>
   )
