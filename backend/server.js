5ee390f374fc53e50e97a846ed44cd0a615b4627
Sakkyx <keigosasaki99@gmail.com>
2026-05-26 22:23:55 -0300

    Corrigindo backend

--- backend/server.js
+++ backend/server.js
@@ -12,21 +12,28 @@ app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB conectado'))
-  .catch(err => console.log(err))
+  .catch(err => console.log('Erro MongoDB:', err))
 
 app.get('/', (req, res) => {
-  res.send('API ok')
+  res.send('API funcionando')
 })
 
 app.post('/clientes', async (req, res) => {
   try {
     const cliente = await Cliente.create(req.body)
+
     res.status(201).json(cliente)
+
   } catch (error) {
-    res.status(500).json({ erro: error.message })
+    res.status(500).json({
+      erro: error.message
+    })
   }
 })
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000')
+// Porta do Render
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
